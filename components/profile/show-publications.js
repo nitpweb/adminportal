a8a0b752dcc78c92c60ca45ed121c8831d0e7333
Neeraj Patil <patilneeraj2212@gmail.com>
2021-05-27 02:03:06 +0530

    display books

--- components/profile/show-publications.js
+++ components/profile/show-publications.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataGrid } from "@material-ui/data-grid";
 
 const ShowPublications = ({ publications }) => {
@@ -7,9 +7,42 @@ const ShowPublications = ({ publications }) => {
 	const [conferences, setConferences] = useState([]);
 	const [patents, setPatents] = useState([]);
 
+	useEffect(() => {
+		let _articles = [],
+			_books = [],
+			_conferences = [],
+			_patents = [];
+
+		publications.forEach((entry, idx) => {
+			entry.id = idx;
+			if (entry.type == "article") _articles.push(entry);
+			else if (entry.type == "book") _books.push(entry);
+			else if (entry.type == "conference") _conferences.push(entry);
+			else if (entry.type == "patent") _patents.push(entry);
+		});
+
+		setArticles(_articles);
+		setConferences(_conferences);
+		setBooks(_books);
+		setPatents(_patents);
+	}, [publications]);
+
+	return (
+		<>
+			<div style={{ marginTop: `50px`, marginBottom: `50px` }}>
+				{articles.length && <Articles articles={articles} />}
+			</div>
+			<div style={{ marginTop: `50px`, marginBottom: `50px` }}>
+				{books.length && <Books books={books} />}
+			</div>
+		</>
+	);
+};
+
+const Articles = ({ articles }) => {
 	const columns = [
 		{ field: "id", headerName: "ID", flex: 0.4 },
-		{ field: "type", headerName: "Type" },
+
 		{ field: "title", headerName: "Title", flex: 1 },
 		{ field: "authors", headerName: "Authors", flex: 1 },
 		{
@@ -20,27 +53,66 @@ const ShowPublications = ({ publications }) => {
 		{ field: "year", headerName: "Year", type: "number" },
 		{ field: "citation_key", headerName: "Citation Key" },
 	];
-	const rows = publications
-		.filter((entry) => entry.type == "article")
-		.map((entry, idx) => {
-			entry.id = idx;
-			return entry;
-		});
+	const rows = articles;
 
-	console.log(rows);
+	let style = {
+		width: "95%",
+	};
+
+	return (
+		<>
+			<h1>Articles</h1>
+			<div style={style}>
+				<DataGrid
+					rows={rows}
+					columns={columns}
+					pageSize={5}
+					pagination
+					autoHeight
+					rowsPerPageOptions={false}
+					checkboxSelection
+					disableSelectionOnClick
+				/>
+			</div>
+		</>
+	);
+};
+
+const Books = ({ books }) => {
+	const columns = [
+		{ field: "id", headerName: "ID", flex: 0.4 },
+		{ field: "title", headerName: "Title", flex: 1 },
+		{ field: "authors", headerName: "Authors", flex: 1 },
+		{
+			field: "editors",
+			headerName: "Editors",
+			sortable: false,
+		},
+		{
+			field: "publisher",
+			headerName: "Publisher",
+			sortable: false,
+		},
+		{ field: "year", headerName: "Year", type: "number" },
+		{ field: "citation_key", headerName: "Citation Key" },
+	];
+	const rows = books;
+
 	return (
 		<>
 			<div
 				style={{
-					height: 400,
 					width: "95%",
-					marginTop: `50px`,
-					marginBottom: `50px`,
 				}}
 			>
+				<h1>Books</h1>
 				<DataGrid
 					rows={rows}
 					columns={columns}
+					pageSize={5}
+					pagination
+					autoHeight
+					rowsPerPageOptions={false}
 					checkboxSelection
 					disableSelectionOnClick
 				/>
@@ -50,24 +122,3 @@ const ShowPublications = ({ publications }) => {
 };
 
 export default ShowPublications;
-
-{
-	/* <table>
-	{publications.map((item) => {
-		return (
-			<li>
-				{item.title}{" "}
-				<IconButton aria-label="delete" onClick={() => addModalOpen10d()}>
-					<DeleteIcon />
-				</IconButton>
-				<ConfirmDelete
-					handleClose={handleCloseAddModal10d}
-					modal={addModal10d}
-					id={item.id}
-					del={"publications"}
-				/>
-			</li>
-		);
-	})}
-</table>; */
-}
