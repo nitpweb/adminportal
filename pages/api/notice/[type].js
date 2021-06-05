3e9119a0ba9cc48b40b7c34f04903d59a0ce6478
Amrit Raj <55765255+dev-amrit460@users.noreply.github.com>
2021-06-05 17:44:09 +0530

    Update [type].js

--- pages/api/notice/[type].js
+++ pages/api/notice/[type].js
@@ -4,6 +4,18 @@ const handler = async (req, res) => {
   const { type } = req.query;
 
   try {
+    const fdept = {
+      arch: "Architecture",
+      che: "Chemistry",
+      ce: "Civil Engineering",
+      cse: "Computer Science and Engineering",
+      ee: "Electrical Engineering",
+      ece: "Electronics and Communication Engineering",
+      hss: "Humanities & Social Sciences",
+      maths: "Mathematics",
+      me: "Mechanical Engineering",
+      phy: "Physics",
+    };
     let results;
     const now = new Date().getTime();
     if (type === "all") {
@@ -13,20 +25,23 @@ const handler = async (req, res) => {
     `
       );
     } else if (type == "active") {
-      results = await query(
-        `
-        SELECT * from notices where openDate<? and closeDate>? and isVisible=1`,
-        [now, now]
-      );
+      results = await query(`
+        SELECT * from notices where openDate<${now} and closeDate>${now} and isVisible=1;
+        `);
     } else if (type == "range") {
       const start = req.body.start_date;
       const end = req.body.end_date;
 
+      results = await query(`
+			SELECT * from notices where closeDate<=${end} and openDate>=${start}`).catch(
+        (err) => console.log(err)
+      );
+    } else if (type in fdept) {
       results = await query(
         `
-			SELECT * from notices where closeDate<=? and openDate>=?`,
-        [end, start]
-      ).catch((err) => console.log(err));
+      select * from notices where isDept=1 and department="${fdept[type]}"
+    `
+      );
     }
     let array = JSON.parse(JSON.stringify(results));
     array.forEach((element) => {
