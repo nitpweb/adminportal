e4519474fda07c9319b4f7ca82ad6deb866d9082
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2022-06-28 17:54:19 +0530

    CV update

---
+++ components/profile/getPdfUrl.js
@@ -0,0 +1,8 @@
+export const getPdfUrl = (url) => {
+  if (url.includes("drive.google.com")) {
+    var id = url.split('id=')[1].split('&')[0]
+    return `https://drive.google.com/file/d/${id}/preview`;
+  } else {
+    return url;
+  }
+};
